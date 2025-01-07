dbf0b8df7ba60e980121fa23409326c9541ae03f
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-01-08 00:09:03 +0530

    Completed End to end Todo App

---
+++ Todo App/src/App.jsx
@@ -0,0 +1,5 @@
+import { Todo } from "./Projects/Todo/Todo";
+
+export const App = () => {
+  return <section><Todo/></section>
+};
---
+++ Todo App/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",                   // Include index.html
+    "./src/**/*.{js,ts,jsx,tsx}",     // Include all JS, TS, JSX, TSX files inside the 'src' directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
